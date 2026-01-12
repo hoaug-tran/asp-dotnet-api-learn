cad0a5e7466513fb202def90eb8de785a91fe699
unknown <hoaug@duck.com>
2026-01-12 11:28:06 +0700

    chore: remove frontend subtree

--- frontend-test/src/List.jsx
+++
@@ -1,9 +0,0 @@
-const List = () => {
-  const fruits = ["apple", "orange", "banana", "coconut", "pineapple"];
-
-  const listItems = fruits.map((f) => <li>{f}</li>);
-
-  return <ul>{listItems}</ul>;
-};
-
-export default List;
--- frontend-test/src/UserGreeting.jsx
+++
@@ -1,9 +0,0 @@
-const UserGreeting = (props) => {
-  return props.isLoggedIn ? (
-    <h2>Welcome {props.username}</h2>
-  ) : (
-    <h2>Please login to continue</h2>
-  );
-};
-
-export default UserGreeting;
